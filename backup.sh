499e77c6155f223d361b7387d9283f6b69245fee
Michael Barsukov <barsukov.m23@gmail.com>
2021-05-23 18:57:52 +0300

    Backup 2021-05-23_18-57

--- backup.sh
+++ backup.sh
@@ -6,8 +6,8 @@ mkdir -p $DOTFILES_DIR/{.config,.local/share}
 
 # Backup the configuration files
 sudo cp -rv ~/{.fehbg,.oh-my-zsh,.vim,.vimrc,.xinitrc,.Xresources,.zshrc} $DOTFILES_DIR
-cp -rv ~/.config/{alacritty,cava,dunst,flameshot,fontconfig,i3,kitty,mimeapps.list,neofetch,nnn,picom,polybar,rofi,zathura} $DOTFILES_DIR/.config
-cp -r ~/.local/share/fonts $DOTFILES_DIR/.local/share
+sudo cp -rv ~/.config/{alacritty,cava,dunst,flameshot,fontconfig,i3,kitty,mimeapps.list,neofetch,nnn,picom,polybar,rofi,zathura} $DOTFILES_DIR/.config
+sudo cp -r ~/.local/share/fonts $DOTFILES_DIR/.local/share
 
 # Check git status
 gs="$(git status | grep -i modified)"
